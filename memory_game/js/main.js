14f229fef0c0d53d0c47412bbdc5e9a41f482b10
fagleyali <fagley.ali@gmail.com>
2019-01-14 13:48:33 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -10,23 +10,25 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[2];
-
-var cardTwo= cards[3];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log(cardsInPlay);
-
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardTwo);
-
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay [1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry! Try again");
 	}
-}
+};
+
+};
+
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+
+};
+
+
+flipCard(0);
+flipCard(2);
+
