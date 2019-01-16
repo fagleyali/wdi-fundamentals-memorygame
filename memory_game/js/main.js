1aaf748499d9f5179a787fb1467e864870388ccf
fagleyali <fagley.ali@gmail.com>
2019-01-15 22:48:16 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,6 +6,8 @@
 
 //alert("Hello friends!");
 
+
+
 var cards = [
 
 {
@@ -34,9 +36,23 @@ var cards = [
 
 ];
 
+
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
 var cardsInPlay = [];
 
+
+
 var checkForMatch = function(){
+
 	if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay [1]) {
 		alert("You found a match!");
@@ -47,17 +63,20 @@ var checkForMatch = function(){
 
 };
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log("Image of the card is: " + cards[cardId].cardImage);
 	console.log("Suit of the card is:  " + cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 
 };
 
 
-flipCard(0);
-flipCard(2);
+// flipCard(0);
+// flipCard(2);
 
+createBoard();
 
